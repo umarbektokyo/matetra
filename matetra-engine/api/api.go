8e9e624d53fd7314a0bbe5c85184ae79700dc43a
Umarik <umarbektokyo@icloud.com>
2025-12-04 22:07:05 +0900

    api, wip

---
+++ matetra-engine/api/api.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"matetra/engine"
+	"net/http"
+)
+
+type API struct {
+	Game *engine.Game
+}
+
+func New(game *engine.Game) *API {
+	return &API{Game: game}
+}
+
+// starts the server + endpoints
+func (a *API) Start() {
+	http.HandleFunc("/state", a.handleState)
+	http.HandleFunc("/add-player", a.handlePlayer)
+
+	log.Println("API running on :1729")
+	log.Fatal(http.ListenAndServe(":1729", nil))
+}
+
+// GET /state
+func (a *API) handleState(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(a.Game.State)
+}
+
+// POST /add-player -> have to implement hashing
+func (a *API) handlePlayer(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "POST required", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body struct {
+		Name string `json:"name"`
+		Hash string `json:"hash"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "invalid JSON", 400)
+		return
+	}
+
+	if err := a.Game.AddPlayer(body.Name, body.Hash); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
